4d67ff9d0f9464c8ee9137ca849c4811274246aa
JoelWhiting <joelwhitingdeangelow@gmail.com>
2024-12-09 00:14:51 -0500

    Add JSDoc comments to Content model

--- models/Content.js
+++ models/Content.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose');
 
+/**
+ * @typedef {Object} Content
+ * @property {string} content - The content to be stored.
+ * @property {Date} timestamp - The date and time when the content was created. Defaults to the current date and time.
+ */
+
+/**
+ * Mongoose schema for storing content.
+ * @constructor Content
+ */
 const contentSchema = new mongoose.Schema({
     content: { type: String, required: true },
     timestamp: { type: Date, default: Date.now },
